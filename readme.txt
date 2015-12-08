17716efdd3359a690b09a0469602b55d058b952c
Sean Du <sean.du@bhnetwork.com>
2015-12-08 15:50:32 -0800

    add MVC support

--- readme.txt
+++ readme.txt
@@ -1,11 +1,20 @@
-1. ssh to the instance, password is generated during the start up time
-ssh -p 2000 user@localhost
-2. scripts/populate.sh to populate some records using http post
-3. http://localhost:8080/txn to display records
-4. property encryption
+1. Custom banner file is located under src/main/resources
+
+2. enabled the ssh to the remote commandline service, to connect 
+	ssh -p 2000 user@localhost
+	password is displayed in the start up console
+	custom command example is under commands/*.groovy
+
+3. to test restful service, create transaction record, use script/populate.sh
+
+4. use http://localhost:8080/txn to display records
+
+5. property encryption, 
+	to generate the encrypted password, use command
 	java -cp ~/.m2/repository/org/jasypt/jasypt/1.9.2/jasypt-1.9.2.jar 
 	org.jasypt.intf.cli.JasyptPBEStringEncryptionCLI input="password" 
 	password=supersecretz algorithm=PBEWithMD5AndDES
  
- add this during the start up of jvm
- -Djasypt.encryptor.password=supersecretz
+ 	add following line during the start up of jvm, to allow Jasypt to know which master password to use.
+ 	-Djasypt.encryptor.password=supersecretz
+ 	
--- src/main/java/com/bhn/BootstrapApplication.java
+++ src/main/java/com/bhn/BootstrapApplication.java
@@ -3,13 +3,12 @@ package com.bhn;
 import org.h2.server.web.WebServlet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 
 
 
@@ -19,20 +18,19 @@ import org.springframework.context.annotation.ComponentScan;
  *
  */
 @SpringBootApplication
-@ComponentScan("commands, com.bhn")
+//@ComponentScan("commands, com.bhn")
 public class BootstrapApplication extends SpringBootServletInitializer{
 	
 	static Logger logger = LoggerFactory.getLogger(BootstrapApplication.class);
 
 	/* entry point for stand alone application */
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
     	SpringApplication app = new SpringApplication(BootstrapApplication.class);
-    	app.setShowBanner(true);
         app.run(args);
-    }
+    }*/
     
     
-    /* entry point for Servlet 3.0 application */
+    /* entry point for Servlet 3.0 application */  
     @Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
     	return application.sources(BootstrapApplication.class); 
